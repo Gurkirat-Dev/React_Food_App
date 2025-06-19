f92b6a2677c1ba00fbb32b1df69cc5996988382e
Gurkirat Singh <GurkiretSINGH@Mac-mini.local>
2025-06-19 18:20:38 -0400

    Updates

---
+++ Grocery.4df89dde.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire3c57;(0,e.register)("loR3J",function(l,t){var r=e("ayMG0");Object.defineProperty(l.exports,"__esModule",{value:!0}),l.exports.default=void 0,l.exports.default=()=>(0,r.jsx)("div",{className:"flex justify-center items-center h-full w-full",children:(0,r.jsx)("h1",{className:"text-9xl mt-50",children:"Coming Soon"})})});
+//# sourceMappingURL=Grocery.4df89dde.js.map
